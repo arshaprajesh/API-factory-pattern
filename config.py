bc03afe4b27b54e65e19806ee7d8dc73834f59a2
arsha <arshababu333@gmail.com>
2025-07-17 14:31:51 -0400

    changed uri

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
-
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI='postgresql://root:CK123indb#1@localhost/mechanic_service'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:CK123indb#1@localhost/mechanic_service'
     DEBUG=True
     CACHE_TYPE = 'SimpleCache'
     CACHE_DEFAULT_TIMEPOUT=300
@@ -13,5 +12,5 @@ class TestingConfig:
     CACHE_TYPE = 'SimpleCache'  
     CACHE_DEFAULT_TIMEPOUT=300  
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI','postgresql://root:CK123indb#1@localhost/mechanic_service')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI','postgresql+psycopg://root:CK123indb#1@localhost/mechanic_service')
     CACHE_TYPE = "SimpleCache"
