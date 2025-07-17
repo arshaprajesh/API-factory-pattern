6fd6679dcf8866ccf3189191bc75079a9251575d
arsha <arshababu333@gmail.com>
2025-07-17 19:50:20 -0400

    Fix: listen on 0.0.0.0 and use PORT env variable

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 
 from app import create_app
 from app.models import db
+import os
 
 app = create_app('ProductionConfig')
 
@@ -8,6 +9,7 @@ with app.app_context():
     #db.drop_all() 
     db.create_all() 
     
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 
 
