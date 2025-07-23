b9e50758a99a972b2a27cd878d0c807bf4b95dc7
arsha <arshababu333@gmail.com>
2025-07-23 13:17:33 -0400

    added support credential

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 
 def create_app(config_name):
     app = Flask(__name__)
-    CORS(app,supports_credentials=True)
+    CORS(app,resources={r"/*": {"origins": "*"}}, supports_credentials=True)
     
     
     
