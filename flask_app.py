a61abcc9ad298c83c164e35ec163f94b941c3cb6
arsha <arshababu333@gmail.com>
2025-07-24 12:24:16 -0400

    add  os

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 
 from app import create_app
 from app.models import db
-
+import os
 
 app = create_app('ProductionConfig')
 
@@ -9,7 +9,7 @@ with app.app_context():
     #db.drop_all() 
     db.create_all() 
     
-#app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
     
 
 
