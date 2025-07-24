b254b85c13de8b914289b999a0f9166c1b20f52a
arsha <arshababu333@gmail.com>
2025-07-24 11:40:14 -0400

    remove run

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 
-""" from app import create_app
+from app import create_app
 from app.models import db
 import os
 
@@ -9,23 +9,3 @@ with app.app_context():
     #db.drop_all() 
     db.create_all() 
     
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
-
- """
-from app import create_app
-from app.models import db
-import os
-
-config_name = os.environ.get('FLASK_CONFIG', 'ProductionConfig')
-app = create_app(config_name)
-
-with app.app_context():
-    # Only use this in dev/testing!
-    # db.drop_all()
-    db.create_all()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
-        
