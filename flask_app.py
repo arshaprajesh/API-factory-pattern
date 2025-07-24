c8853a2579992723621187863ef90155797c5e63
arsha <arshababu333@gmail.com>
2025-07-24 11:29:33 -0400

    add config

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 
-from app import create_app
+""" from app import create_app
 from app.models import db
 import os
 
@@ -12,6 +12,20 @@ with app.app_context():
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 
+ """
+from app import create_app
+from app.models import db
+import os
 
+config_name = os.environ.get('FLASK_CONFIG', 'ProductionConfig')
+app = create_app(config_name)
+
+with app.app_context():
+    # Only use this in dev/testing!
+    # db.drop_all()
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
         
