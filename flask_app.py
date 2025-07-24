52b2ac66153a8a2ee160405d33e8862db7b4bfbb
arsha <arshababu333@gmail.com>
2025-07-24 11:51:03 -0400

    remove run

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 
-from app import create_app
+""" from app import create_app
 from app.models import db
 import os
 
@@ -9,3 +9,22 @@ with app.app_context():
     #db.drop_all() 
     db.create_all() 
     
+
+
+
+ """
+from app import create_app
+from app.models import db
+import os
+
+config_name = os.environ.get('FLASK_CONFIG', 'ProductionConfig')
+app = create_app(config_name)
+
+with app.app_context():
+   
+    # db.drop_all()
+    db.create_all()
+
+
+
+        
