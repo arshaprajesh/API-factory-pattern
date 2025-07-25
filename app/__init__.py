43446f919d05e0a97f72b6321adb9bf1646c16e2
arsha <arshababu333@gmail.com>
2025-07-25 15:43:56 -0400

    changed init file

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from .bluePrints.serviceTickets import serviceTickets_bp
 from .bluePrints.inventory import inventory_bp
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS 
-from flask import send_from_directory
+
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = 'https://api-factory-pattern.onrender.com/static/swagger.yaml'  # Our API URL (can of course be a local resource)
@@ -23,15 +23,6 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 def create_app(config_name):
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-
-  
-    @app.route('/static/<path:filename>')
-    def serve_static(filename):
-        response = send_from_directory('static', filename)
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        return response
-
    
     app.config.from_object('config.' + config_name)
     
@@ -40,11 +31,7 @@ def create_app(config_name):
     db.init_app(app)
     limiter.init_app(app)
     cache.init_app(app)
-    #=============================
-    CORS(customers_bp)
-    CORS(mechanics_bp)
-    CORS(serviceTickets_bp)
-    CORS(inventory_bp)
+    
     
      #=====register blue prints=======
     app.register_blueprint(customers_bp,url_prefix = '/customers')
